c7fa15c3ea7bc63ff7ff13149863025b4bd03d8b
Muhammet Baskurt <67770452+mbkurt06@users.noreply.github.com>
2022-08-04 15:24:14 +0200

    Update Manav.java

--- src/day01_manavprojesi/Manav.java
+++ src/day01_manavprojesi/Manav.java
@@ -12,41 +12,9 @@ public class Manav {
     public static void main(String[] args) {
 
 
-
-        System.out.println(" No  Ürün    Fiyat\n" +
-                " == ======= =======\n" +
-                " 00 Domates 2.10 TL\n" +
-                " 01 Patates 3.20 TL\n" +
-                " 02 Biber   1.50 TL\n" +
-                " 03 Soğan   2.30 TL\n" +
-                " 04 Havuç   3.10 TL\n" +
-                " 05 Elma    1.20 TL\n" +
-                " 06 Muz     1.90 TL\n" +
-                " 07 Çilek   6.10 TL\n" +
-                " 08 Kavun   4.30 TL\n" +
-                " 09 Üzüm    2.70 TL\n" +
-                " 10 Limon   0.50 TL\n");
-        System.out.println("urunlerimiz");
-        //Adem Bey
-
-        sepeteEklenenUrunler(sepetUrunIsmi);
-        sepetUrunFiyatilari(sepetUrunFiyati);
-        sepetUrunlerKg(sepetUrunKg);
-
-
-        /*
-        sepetUrunler  list(string), sepetKg(double), sepetFiyat(double)
-       tum islemleri methodlara yaptır.
-       method--> public static void sepeteEkle(int ürünNo, double sepetKg){}
-       alısverise devam mı ödeme mi?
-       (1- method olarak yapılacak, listeler class seviyesinde, methodlara parametrer
-       olarak seçilen ürünün cins kilosu gibi bilgiler verilecek
-         */
-
-
-
     }
 
+    
     public static void sepetUrunlerKg(List<Double> sepetUrunKg) {
         sepetUrunKg.add(1.5);
 
