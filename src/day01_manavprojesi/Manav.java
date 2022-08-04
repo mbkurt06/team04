f104052359a680000a335bee9d8c7b85d0c7dca0
Muhammet Baskurt <mbkurt06@gmail.com>
2022-08-04 15:53:41 +0200

    fatura methodu eklendi

--- src/day01_manavprojesi/Manav.java
+++ src/day01_manavprojesi/Manav.java
@@ -5,16 +5,15 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Manav {
-    static List<String> sutUrunleriName =new ArrayList<>();//sepetteki urunlerin ismini tutan liste
-    static List<Double> sepetUrunFiyati= new ArrayList<>();// sepetteki urunlerin fiyati
-    static List<Double> sepetUrunKg= new ArrayList<>();
-    static List<String> sepetUrunIsmi= new ArrayList<>();
+    static List<String> sutUrunleriName = new ArrayList<>();//sepetteki urunlerin ismini tutan liste
+    static List<Double> sepetUrunFiyati = new ArrayList<>();// sepetteki urunlerin fiyati
+    static List<Double> sepetUrunKg = new ArrayList<>();
+    static List<String> sepetUrunIsmi = new ArrayList<>();
     static Scanner scan = new Scanner(System.in);
 
 
-
-    public static List<String> manavUrunler(){ //Manav urunleri icin liste
-        List<String> manavName=new ArrayList<>();
+    public static List<String> manavUrunler() { //Manav urunleri icin liste
+        List<String> manavName = new ArrayList<>();
         manavName.add("Domates");
         manavName.add("Patates");
         manavName.add("Biber");
@@ -28,8 +27,9 @@ public class Manav {
 
         return manavUrunler();
     }
-    public static List<Double>manavFiyat(){  // Manav fiyatlari icin liste
-        List<Double> manavFiy=new ArrayList<>();
+
+    public static List<Double> manavFiyat() {  // Manav fiyatlari icin liste
+        List<Double> manavFiy = new ArrayList<>();
         manavFiy.add(2.10);
         manavFiy.add(3.20);
         manavFiy.add(1.50);
@@ -44,8 +44,8 @@ public class Manav {
         return manavFiyat();
     }
 
-    public static List<String> sarkuteriUrunler(){   //sarkuteri icin liste
-        List<String> sarkuteriName=new ArrayList<>();
+    public static List<String> sarkuteriUrunler() {   //sarkuteri icin liste
+        List<String> sarkuteriName = new ArrayList<>();
         sarkuteriName.add("Pastirma");
         sarkuteriName.add("Sucuk");
         sarkuteriName.add("Selam");
@@ -57,8 +57,8 @@ public class Manav {
         return sarkuteriUrunler();
     }
 
-    public static List<Double> sarkuteriFiyatlar(){ // sarkuteri fiyatlari bulunan liste
-        List<Double> sarkFiy=new ArrayList<>();
+    public static List<Double> sarkuteriFiyatlar() { // sarkuteri fiyatlari bulunan liste
+        List<Double> sarkFiy = new ArrayList<>();
         sarkFiy.add(8.40);
         sarkFiy.add(4.80);
         sarkFiy.add(7.90);
@@ -70,8 +70,8 @@ public class Manav {
         return sarkuteriFiyatlar();
     }
 
-    public static List<String> sutUrunler(){   //sut urunleri icin liste
-        List<String> sutUrunleriName=new ArrayList<>();
+    public static List<String> sutUrunler() {   //sut urunleri icin liste
+        List<String> sutUrunleriName = new ArrayList<>();
         sutUrunleriName.add("Sut");
         sutUrunleriName.add("Beyaz Peynir");
         sutUrunleriName.add("Suzme");
@@ -83,8 +83,8 @@ public class Manav {
         return sutUrunler();
     }
 
-    public static List<Double> sutFiyatlar(){ //sut urunlerinin fiyati icin liste
-        List<Double>sutF=new ArrayList<>();
+    public static List<Double> sutFiyatlar() { //sut urunlerinin fiyati icin liste
+        List<Double> sutF = new ArrayList<>();
         sutF.add(13.00);
         sutF.add(17.00);
         sutF.add(12.00);
@@ -95,7 +95,8 @@ public class Manav {
 
         return sarkuteriFiyatlar();
     }
-    public static void printManav(){
+
+    public static void printManav() {
         System.out.println(" No  Ürün    Fiyat\n" +
                 " == ======= =======\n" +
                 " 00 Domates 2.10 TL\n" +
@@ -111,7 +112,7 @@ public class Manav {
                 " 10 Limon   0.50 TL\n");
     }
 
-    public static void printSarkuteri(){
+    public static void printSarkuteri() {
         System.out.println(" No  Ürün    Fiyat\n" +
                 " == ======= =======\n" +
                 " 00 Pastirma  2.10 TL\n" +
@@ -120,12 +121,12 @@ public class Manav {
                 " 03 Sosis     2.30 TL\n" +
                 " 04 Kavurma   3.10 TL\n" +
                 " 05 Duman     1.20 TL\n" +
-                " 06 Jambon    1.90 TL\n" );
+                " 06 Jambon    1.90 TL\n");
 
 
     }
 
-    public static void printSutUrun(){
+    public static void printSutUrun() {
         System.out.println(" No  Ürün    Fiyat\n" +
                 " == ======= =======\n" +
                 " 00 Sut           2.10 TL\n" +
@@ -134,10 +135,10 @@ public class Manav {
                 " 03 Yogurt        2.30 TL\n" +
                 " 04 Kefir         3.10 TL\n" +
                 " 05 Burma peynir  1.20 TL\n" +
-                " 06 Ayran         1.90 TL\n" );
+                " 06 Ayran         1.90 TL\n");
     }
 
- public static void sepeteUrunMiktariEkle(Double miktar) {
+    public static void sepeteUrunMiktariEkle(Double miktar) {
         sepetUrunKg.add(miktar);
 
     }
@@ -149,22 +150,44 @@ public class Manav {
     public static void sepeteUrunAdiEkle(String urunAdi) {
         sepetUrunIsmi.add(urunAdi);
     }
+    public static void getFatura(List<String> sepetUrunIsmi, List<Double> sepetUrunFiyati,List<Double> sepetUrunKg){
 
+    }
 
     public static void main(String[] args) {
-        System.out.println("     ==Gida Marketimize Hoş Geldiniz!==");
-        System.out.println("Asagidaki sayilar ile istediginiz urunlere gidin \n" +
-                "   1 - Manav\n   2 - Sharkuteri\n   3 - Sut urunleri  \nSeciminiz: ");
-        Scanner sc = new Scanner(System.in);
-        int secim = sc.nextInt();
-        if (secim == 1) {           // Kullanici ilk basta hangi urun paketine gidecekse burdan secim yapar
-            printManav();
-        } else if (secim == 2) {
-            printSarkuteri();
-        } else if (secim == 3) {
-            printSutUrun();
-        } else
-            System.out.println("Yanlis secim yaptiniz");
+        boolean alisVeriseDevamMi = true;
+        int secim = 0;
+        int urunSecim = 0;
+        int cikis;
+
+        while (alisVeriseDevamMi) {
+            secim = 0;
+            cikis = 1;
+
+            System.out.println("     ==Gida Marketimize Hoş Geldiniz!==");
+            System.out.println("Asagidaki sayilar ile istediginiz urunlere gidin \n" +
+                    "   1 - Manav\n   2 - Sharkuteri\n   3 - Sut urunleri\n   4 - Kasa\n   5 - Cikis \nSeciminiz: ");
+            Scanner sc = new Scanner(System.in);
+            secim = sc.nextInt();
+            if (secim == 1) // Kullanici ilk basta hangi urun paketine gidecekse burdan secim yapar
+            {
+                while (cikis != 0) {
+                    printManav();
+                    System.out.println("Bir ust menuye gecmek icin 0 a basiniz");
+                    urunSecim = scan.nextInt();
+                }
+
+            } else if (secim == 2) {
+                printSarkuteri();
+            } else if (secim == 3) {
+                printSutUrun();
+            } else if (secim == 4) {
+                getFatura();
+            } else if (secim == 5) {
+                alisVeriseDevamMi = false;
+            } else
+                System.out.println("Yanlis secim yaptiniz");
+        }
     }
 
 }
