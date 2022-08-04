55e8d1f15eb8aba49b36531421bb7418875a4fad
Muhammet Baskurt <67770452+mbkurt06@users.noreply.github.com>
2022-08-04 15:23:06 +0200

    Update Manav.java

--- src/day01_manavprojesi/Manav.java
+++ src/day01_manavprojesi/Manav.java
@@ -11,53 +11,22 @@ public class Manav {
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
 
-        System.out.println("adem branch");
-
-        System.out.println(" No  Ürün    Fiyat\n" +
-                " == ======= =======\n" +
-                " 00 Domates 2.10 TL\n" +
-                " 01 Patates 3.20 TL\n" +
-                " 02 Biber   1.50 TL\n" +
-                " 03 Soğan   2.30 TL\n" +
-                " 04 Havuç   3.10 TL\n" +
-                " 05 Elma    1.20 TL\n" +
-                " 06 Muz     1.90 TL\n" +
-                " 07 Çilek   6.10 TL\n" +
-                " 08 Kavun   4.30 TL\n" +
-                " 09 Üzüm    2.70 TL\n" +
-                " 10 Limon   0.50 TL\n");
-        System.out.println("urunlerimiz");
-        //Adem Bey
-
-        sepeteEklenenUrunler(sepetUrunIsmi);
-        sepetUrunFiyatilari(sepetUrunFiyati);
-        sepetUrunlerKg(sepetUrunKg);
-
-
-        /*
-        sepetUrunler  list(string), sepetKg(double), sepetFiyat(double)
-       tum islemleri methodlara yaptır.
-       method--> public static void sepeteEkle(int ürünNo, double sepetKg){}
-       alısverise devam mı ödeme mi?
-       (1- method olarak yapılacak, listeler class seviyesinde, methodlara parametrer
-       olarak seçilen ürünün cins kilosu gibi bilgiler verilecek
-         */
-
+       
 
 
     }
 
-    public static void sepetUrunlerKg(List<Double> sepetUrunKg) {
-        sepetUrunKg.add(1.5);
+    public static void sepeteUrunMiktariEkle(Double miktar) {
+        sepetUrunKg.add(miktar);
 
     }
 
-    public static void sepetUrunFiyatilari(List<Double> sepetUrunFiyati) {
-        sepetUrunFiyati.add(2.1);
+    public static void sepeteUrunFiyatiEkle(Double price) {
+        sepetUrunFiyati.add(price);
     }
 
-    public static void sepeteEklenenUrunler(List<String> sepetUrunIsmi) {
-        sepetUrunIsmi.add("Domates");
+    public static void sepeteUrunAdiEkle(String urunAdi) {
+        sepetUrunIsmi.add(urunAdi);
     }
 
 
