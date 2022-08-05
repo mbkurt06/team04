d9475b3f66c2f4548e6d1318e35eafad8141e043
ademk7604 <104134174+ademk7604@users.noreply.github.com>
2022-08-05 20:10:57 +0200

    ada

--- src/day01_manavprojesi/Manav.java
+++ src/day01_manavprojesi/Manav.java
@@ -8,29 +8,6 @@ public class Manav {
     static List<Integer> sepetUrunNo=new ArrayList<>();//sepetteki urunlerin ismini tutan liste
     static List<Double> sepetUrunFiyati= new ArrayList<>();// sepetteki urunlerin fiyati
     static List<Double> sepetUrunKg= new ArrayList<>();
-    static Scanner scan = new Scanner(System.in);
-    public static void main(String[] args) {
-
-
-        System.out.println("adem branch");
-
-        System.out.println(" No  Ürün    Fiyat\n" +
-                " == ======= =======\n" +
-                " 00 Domates 2.10 TL\n" +
-                " 01 Patates 3.20 TL\n" +
-                " 02 Biber   1.50 TL\n" +
-                " 03 Soğan   2.30 TL\n" +
-                " 04 Havuç   3.10 TL\n" +
-                " 05 Elma    1.20 TL\n" +
-                " 06 Muz     1.90 TL\n" +
-                " 07 Çilek   6.10 TL\n" +
-                " 08 Kavun   4.30 TL\n" +
-                " 09 Üzüm    2.70 TL\n" +
-                " 10 Limon   0.50 TL\n");
-        System.out.println("urunlerimiz");
-        //Adem Bey
-
-
 
 
         /*
@@ -42,10 +19,6 @@ public class Manav {
        olarak seçilen ürünün cins kilosu gibi bilgiler verilecek
          */
 
-
-
-
-    }
 
     public static void sepeteUrunMiktariEkle(Double miktar) {
         sepetUrunKg.add(miktar);
@@ -62,7 +35,6 @@ public class Manav {
     }
     public static void sepeteUrunAdiEkle(String urunAdi) {
 
-        System.out.println("Adem");
 
     }
 
